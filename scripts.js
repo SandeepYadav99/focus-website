30794e4fac87a86fc34ed04c0cb2e39ce5144650
sandeep.electrovese <sandeepelectrovese@gmail.com>
2024-02-21 19:23:27 +0530

    update Scripts

--- scripts.js
+++ scripts.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:10,
-        responsiveClass:true,
+      
         center:true,
-        loop:true,
+        
         responsive:{
             0:{
                 items:1,
